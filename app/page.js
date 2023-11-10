83e99eb74d3a939799b8e591d123cc44b48afb84
kanbei0605 <kanbei0605@gmail.com>
2023-11-11 01:23:23 +0900

    updated style

--- app/page.js
+++ app/page.js
@@ -3,11 +3,9 @@ import styles from "../styles/main.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.game}>
-        <h1>Sudoku Solver</h1>
-        <Sudoku />
-      </div>
+    <div className={styles.game}>
+      <h1>Sudoku Solver</h1>
+      <Sudoku />
     </div>
   );
 }
