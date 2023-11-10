466e3ad217b2817311a408517643d59dfde612d3
kanbei0605 <kanbei0605@gmail.com>
2023-11-10 21:14:04 +0900

    install next.js & scss

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import styles from "../styles/main.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <p>Sudoku Solution</p>
+    </main>
+  );
+}
